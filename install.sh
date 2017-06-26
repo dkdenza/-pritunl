890c9ee85c41dbbc4c000ca0cd6aab13df267a2c
เฮียแงะไงจะใครล่ะ <beer_lonely@hotmail.com>
2017-06-26 23:57:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ service pritunl start
 echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 apt-get update
-apt-get install pritunl-client
+apt-get install pritunl-client -y
 clear
 
 # Install Squid
